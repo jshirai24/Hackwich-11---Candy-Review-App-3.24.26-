c6a5aea2550ff800889ee422dc375805f1e73374
Jillian Shirai <jtshirai@hawaii.edu>
2026-03-24 11:14:21 -1000

    Initial Commit

---
+++ Hackwich 11 - Candy Review App (3.24.26)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackwich 11 - Candy Review App (3.24.26)
+//
+//  Created by Jillian Shirai on 3/24/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
