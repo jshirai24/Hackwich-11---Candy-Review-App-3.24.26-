ceb93de81f47596f6faa611b0b0fe50c0486b983
Jillian Shirai <jtshirai@hawaii.edu>
2026-04-03 10:49:54 -1000

    Completed the second tab and started to work on the third tab

---
+++ Hackwich 11 - Candy Review App (3.24.26)/HardCandyDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HardCandyDetailViewController.swift
+//  Hackwich 11 - Candy Review App (3.24.26)
+//
+//  Created by Jillian Shirai on 4/3/26.
+//
+
+import UIKit
+
+class HardCandyDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
